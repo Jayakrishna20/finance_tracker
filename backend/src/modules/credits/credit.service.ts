e166788fc6e959eb2ffa2b380b08cec210326346
jayakrishna20 <jayakrishnans2041@gmail.com>
2026-03-03 23:28:18 +0530

    feat: created CRUD apis for credits table

---
+++ backend/src/modules/credits/credit.service.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+import { CreateCreditInput, UpdateCreditInput } from './credit.schema.js';
+
+export class CreditService {
+    constructor(private prisma: PrismaClient) { }
+
+    async createCredit(data: CreateCreditInput) {
+        return this.prisma.credits.create({
+            data,
+        });
+    }
+
+    async getAllCredits() {
+        return this.prisma.credits.findMany({
+            include: {
+                category: {
+                    select: {
+                        categoryId: true,
+                        categoryName: true,
+                        categoryColorCode: true,
+                    },
+                },
+            },
+            orderBy: { createdAt: 'desc' },
+        });
+    }
+
+    async getCreditById(creditId: bigint) {
+        return this.prisma.credits.findUnique({
+            include: {
+                category: {
+                    select: {
+                        categoryId: true,
+                        categoryName: true,
+                        categoryColorCode: true,
+                    },
+                },
+            },
+            where: { creditId },
+        });
+    }
+
+    async updateCredit(creditId: bigint, data: UpdateCreditInput) {
+        return this.prisma.credits.update({
+            where: { creditId },
+            data,
+        });
+    }
+
+    async deleteCredit(creditId: bigint) {
+        return this.prisma.credits.delete({
+            where: { creditId },
+        });
+    }
+}
